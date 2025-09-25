5a0f47f4be8d4f1d4a3e0bb9fc24020df38caa31
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2025-09-26 00:29:52 +0200

    Converted ESX legacy cargobob magnet script to QBCore
    - Migrated entire cargobob magnet logic from ESX Legacy to QBCore framework
    - Replaced all ESX-specific calls with QBCore equivalents
    - Job check now uses QBCore.Functions.GetPlayerData()
    - Hook deploy and vehicle attach/detach logic preserved
    - Notification system updated to use QBCore.Functions.Notify
    - Fully functional for 'mechanic' job using Cargobob variants
    - Cleaned up and simplified native usage for better control and performance

--- nblbnk_cargobob-magnetic/client.lua
+++ nblbnk_cargobob-magnetic/client.lua
@@ -1,37 +1,17 @@
---[[
-######################################################################################
-#  _  _   ___   ___   ___   _      ___     _     _  _   _  __                   _    #
-# | \| | | __| | _ ) | __| | |    | _ )   /_\   | \| | | |/ /      _ _    ___  | |_  #
-# | .` | | _|  | _ \ | _|  | |__  | _ \  / _ \  | .` | | ' <   _  | ' \  / -_) |  _| #
-# |_|\_| |___| |___/ |___| |____| |___/ /_/ \_\ |_|\_| |_|\_\ (_) |_||_| \___|  \__| #
-######################################################################################
-# # # # # # # # # # # # # # # # # # # # # # # # # # #
-#    ___ _____  __  _    ___ ___   _   _____   __   #
-#   | __/ __\ \/ / | |  | __/ __| /_\ / __\ \ / /   #
-#   | _|\__ \>  <  | |__| _| (_ |/ _ \ (__ \ V /    #
-#   |___|___/_/\_\ |____|___\___/_/ \_\___| |_|     #
-#####################################################
-]]--
+--######################################################################################
+--#  _  _   ___   ___   ___   _      ___     _     _  _   _  __                   _    #
+--# | \| | | __| | _ ) | __| | |    | _ )   /_\   | \| | | |/ /      _ _    ___  | |_  #
+--# | .` | | _|  | _ \ | _|  | |__  | _ \  / _ \  | .` | | ' <   _  | ' \  / -_) |  _| #
+--# |_|\_| |___| |___/ |___| |____| |___/ /_/ \_\ |_|\_| |_|\_\ (_) |_||_| \___|  \__| #
+--#																					 #
+--######################################################################################
 
----Key: https://docs.fivem.net/game-references/controls/
+local QBCore = exports['qb-core']:GetCoreObject()
+
 local Keys = {
-	["ESC"] = 322, 		["F1"] = 288, 		["F2"] = 289, 		["F3"] = 170, 		["F5"] = 166, 
-	["F6"] = 167, 		["F7"] = 168, 		["F8"] = 169, 		["F9"] = 56, 		["F10"] = 57,
-	["~"] = 243, 		["1"] = 157, 		["2"] = 158, 		["3"] = 160, 		["4"] = 164, 
-	["5"] = 165,		["6"] = 159, 		["7"] = 161, 		["8"] = 162, 		["9"] = 163, 
-	["-"] = 84, 		["="] = 83, 		["TAB"] = 37,		["Q"] = 44, 		["W"] = 32, 
-	["E"] = 38, 		["R"] = 45, 		["T"] = 245, 		["Y"] = 246, 		["U"] = 303, 
-	["P"] = 199, 		["["] = 39, 		["]"] = 40, 		["ENTER"] = 18, 	["CAPS"] = 137, 
-	["A"] = 34, 		["S"] = 8, 		["D"] = 9, 		["F"] = 23, 		["G"] = 47, 
-	["H"] = 74, 		["K"] = 311, 		["L"] = 182, 		["LEFTSHIFT"] = 21, 	["Z"] = 20, 
-	["X"] = 73, 		["C"] = 26, 		["V"] = 0, 		["B"] = 29,		["N"] = 249, 
-	["M"] = 244, 		[","] = 82, 		["."] = 81, 		["LEFTCTRL"] = 36, 	["LEFTALT"] = 19,
-	["SPACE"] = 22,		["RIGHTCTRL"] = 70, 	["HOME"] = 213, 	["PAGEUP"] = 10, 	["PAGEDOWN"] = 11,
-	["DELETE"] = 178, 	["LEFT"] = 174,		["RIGHT"] = 175, 	["TOP"] = 27, 		["DOWN"] = 173, 
-	["NENTER"] = 201, 	["N4"] = 108, 		["N5"] = 60, 		["N6"] = 107,		["BACKSPACE"] = 177,
-	["N+"] = 96, 		["N-"] = 97, 		["N7"] = 117, 		["N8"] = 61, 		["N9"] = 118
+    ["H"] = 74, -- Magnet raus/rein
+    ["E"] = 38  -- Magnet an/aus
 }
-
 
 local bobs = {
     GetHashKey("CARGOBOB"),
@@ -40,68 +20,73 @@ local bobs = {
     GetHashKey("CARGOBOB4"),
 }
 
-local MAX_MAGNET_STRENGTH = 5000
-local MAX_PULL_STRENGTH = 5000
-local MAX_PullRopeLength = 1000
-
-local magnetActive = false
+local magnetVisible = false
 local magnetPower = false
-
+local attachedVehicle = nil
 local playerJob = nil
 
+-- Job Check
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(10000) -- Check job every 10 seconds.
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        if ESX then
-            local playerData = ESX.GetPlayerData()
-            if playerData and playerData.job then
-                playerJob = playerData.job.name
-            end
+        Citizen.Wait(10000)
+        local playerData = QBCore.Functions.GetPlayerData()
+        if playerData and playerData.job then
+            playerJob = playerData.job.name
         end
     end
 end)
 
+-- Steuerung
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        if playerJob == 'mechanic' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            if IsControlJustPressed(1, 74) then
+        local ped = PlayerPedId()
+        local veh = GetVehiclePedIsUsing(ped)
+
+        if playerJob == "mechanic" then
+            -- Haken ein/aus
+            if IsControlJustPressed(1, Keys["H"]) then
                 for i = 1, #bobs do
-                    local rightveh = IsVehicleModel(veh, bobs[i])
-                    if rightveh then
-                        if not magnetActive then
-                            Citizen.InvokeNative(0x7BEB0C7A235F6F3B, veh, 1)
-                            Citizen.InvokeNative(0x9A665550F8DA349B, veh, false)
-                            magnetActive = true
+                    if IsVehicleModel(veh, bobs[i]) then
+                        if not magnetVisible then
+                            Citizen.InvokeNative(0x7BEB0C7A235F6F3B, veh, 1) -- Haken zeigen
+                            magnetVisible = true
+                            QBCore.Functions.Notify("Magnet ausgefahren", "success")
                         else
-                            Citizen.InvokeNative(0x9A665550F8DA349B, veh, false)
-                            Citizen.InvokeNative(0x9768CF648F54C804, veh)
-                            magnetActive = false
-                            magnetPower = false
+                            if attachedVehicle then
+                                DetachEntity(attachedVehicle, true, true)
+                                attachedVehicle = nil
+                                magnetPower = false
+                            end
+                            Citizen.InvokeNative(0x9768CF648F54C804, veh) -- Haken entfernen
+                            magnetVisible = false
+                            QBCore.Functions.Notify("Magnet eingefahren", "error")
                         end
                     end
                 end
-            elseif IsControlJustPressed(1, 36) and IsControlJustPressed(1, 38) and magnetActive then
-                if not magnetPower then
-                    Citizen.InvokeNative(0x9A665550F8DA349B, veh, true)
-                    Citizen.InvokeNative(0xBCBFCD9D1DAC19E2, veh, MAX_MAGNET_STRENGTH)
-                    Citizen.InvokeNative(0xA17BAD153B51547E, veh, MAX_MAGNET_STRENGTH)
-                    Citizen.InvokeNative(0x685D5561680D088B, veh, MAX_MAGNET_STRENGTH)
-                    Citizen.InvokeNative(0x6D8EAC07506291FB, veh, MAX_PullRopeLength)
-                    Citizen.InvokeNative(0xED8286F71A819BAA, veh, MAX_PULL_STRENGTH)
-                    
-                    local attachedVeh = Citizen.InvokeNative(0x873B82D42AC2B9E5, veh)
-                    if attachedVeh and attachedVeh ~= 0 then
-                        Citizen.InvokeNative(0x89F149B6131E57DA, attachedVeh, false)
+            end
+
+            -- Magnet AN/AUS mit E
+            if IsControlJustPressed(1, Keys["E"]) and magnetVisible then
+                if not magnetPower and attachedVehicle == nil then
+                    local pos = GetEntityCoords(veh)
+                    local foundVeh = GetClosestVehicle(pos.x, pos.y, pos.z - 5.0, 5.0, 0, 70)
+                    if foundVeh and foundVeh ~= 0 then
+                        AttachEntityToEntity(foundVeh, veh, GetEntityBoneIndexByName(veh, "hook_arm"),
+                            0.0, 0.0, -5.0,
+                            0.0, 0.0, 0.0,
+                            true, true, false, false, 2, true)
+                        attachedVehicle = foundVeh
+                        magnetPower = true
+                        QBCore.Functions.Notify("Fahrzeug angezogen", "primary")
+                    else
+                        QBCore.Functions.Notify("Kein Fahrzeug gefunden", "error")
                     end
-                    magnetPower = true
                 else
-                    Citizen.InvokeNative(0x9A665550F8DA349B, veh, false)
-                    local attachedVeh = Citizen.InvokeNative(0x873B82D42AC2B9E5, veh)
-                    if attachedVeh and attachedVeh ~= 0 then
-                        Citizen.InvokeNative(0x89F149B6131E57DA, attachedVeh, true)
+                    if attachedVehicle then
+                        DetachEntity(attachedVehicle, true, true)
+                        QBCore.Functions.Notify("Fahrzeug freigegeben", "primary")
+                        attachedVehicle = nil
                     end
                     magnetPower = false
                 end
@@ -110,19 +95,13 @@ Citizen.CreateThread(function()
     end
 end)
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        if playerJob == 'mechanic' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            if IsControlPressed(0, 0xA0) and IsControlJustPressed(0, 0x4F) then
-                if Citizen.InvokeNative(0x6E08BF5B3722BAC9, veh) or Citizen.InvokeNative(0x1821D91AD4B56108, veh) then
-                    local vehattached = Citizen.InvokeNative(0x873B82D42AC2B9E5, veh)
-                    Citizen.InvokeNative(0x9A665550F8DA349B, veh, false)
-                    Citizen.InvokeNative(0xADF7BE450512C12F, vehattached)
-                    Citizen.InvokeNative(0x9768CF648F54C804, veh)
-                end
-            end
-        end
+-- Notfall-Entfernung per Befehl
+RegisterCommand("removehook", function()
+    local veh = GetVehiclePedIsUsing(PlayerPedId())
+    if Citizen.InvokeNative(0x6E08BF5B3722BAC9, veh) or Citizen.InvokeNative(0x1821D91AD4B56108, veh) then
+        local vehattached = Citizen.InvokeNative(0x873B82D42AC2B9E5, veh)
+        Citizen.InvokeNative(0xADF7BE450512C12F, vehattached)
+        Citizen.InvokeNative(0x9768CF648F54C804, veh)
+        QBCore.Functions.Notify("Haken manuell entfernt", "error")
     end
-end)
+end, false)
